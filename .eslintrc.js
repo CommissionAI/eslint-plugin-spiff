2e811c1b7752193e5770f40896d81e37fb56c11a
Joe Tanner <joe.j.tanner@gmail.com>
2020-03-28 14:13:59 -0600

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,45 @@
 "use strict";
 
 module.exports = {
-    root: true,
-    plugins: [
-        "eslint-plugin"
+  root: true,
+  plugins: ["eslint-plugin"],
+  extends: [
+    "eslint",
+    "plugin:eslint-plugin/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "eslint-plugin/consistent-output": "error",
+    "eslint-plugin/no-deprecated-context-methods": "error",
+    "eslint-plugin/prefer-output-null": "error",
+    "eslint-plugin/prefer-placeholders": "error",
+    "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
+    "eslint-plugin/require-meta-type": "error",
+    "eslint-plugin/test-case-property-ordering": [
+      "error",
+      [
+        "filename",
+        "code",
+        "output",
+        "options",
+        "parser",
+        "parserOptions",
+        "globals",
+        "env",
+        "errors",
+      ],
     ],
-    extends: [
-        "eslint",
-        "plugin:eslint-plugin/recommended"
-    ],
-    rules: {
-        "eslint-plugin/consistent-output": "error",
-        "eslint-plugin/no-deprecated-context-methods": "error",
-        "eslint-plugin/prefer-output-null": "error",
-        "eslint-plugin/prefer-placeholders": "error",
-        "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
-        "eslint-plugin/require-meta-type": "error",
-        "eslint-plugin/test-case-property-ordering": ["error", [
-            "filename",
-            "code",
-            "output",
-            "options",
-            "parser",
-            "parserOptions",
-            "globals",
-            "env",
-            "errors"
-        ]],
-        "eslint-plugin/test-case-shorthand-strings": "error"
+    "eslint-plugin/test-case-shorthand-strings": "error",
+
+    // disabled
+    "require-jsdoc": "off",
+  },
+  overrides: [
+    {
+      files: "scripts/**/*.js",
+      rules: {
+        "no-console": "off",
+      },
     },
-    overrides: [
-        {
-            files: "scripts/**/*.js",
-            rules: {
-                "no-console": "off"
-            }
-        }
-    ]
+  ],
 };
